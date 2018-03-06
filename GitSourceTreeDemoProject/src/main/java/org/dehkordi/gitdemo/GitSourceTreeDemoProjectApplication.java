08ab8d591f69508aebcf52bcdabe6b90280ae444
mahmud <mahmud.dehkordi@gmail.com>
2018-03-05 22:12:58 -0500

    initial state of project Spring Boot GitSourceTreeDemoProject

---
+++ GitSourceTreeDemoProject/src/main/java/org/dehkordi/gitdemo/GitSourceTreeDemoProjectApplication.java
@@ -0,0 +1,12 @@
+package org.dehkordi.gitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSourceTreeDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSourceTreeDemoProjectApplication.class, args);
+	}
+}
